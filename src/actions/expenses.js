d0568c397eb15b80948c27c6e53baa4a290cf4d8
Oleg Kolisnyk <k4loki@gmail.com>
2018-03-12 22:04:38 +0200

    Setup startRemoveExpense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -33,6 +33,18 @@ export const removeExpense = ({id} = {}) => ({
   id
 });
 
+export const startRemoveExpense = ({id} = {}) => {
+  return (dispatch) => {
+    return database.ref(`expenses/${id}`).remove()
+      .then(() => {
+        dispatch(removeExpense({id}));
+      })
+      .catch((err) => {
+        
+      });
+  };
+};
+
 ///EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
   type: 'EDIT_EXPENSE',
